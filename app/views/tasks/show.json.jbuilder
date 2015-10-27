89c8336cece34bcb9ee69655abaf073eefad6a3a
Dinh Xuan Son <dinh.xuan.son@framgia.com>
2015-10-27 15:02:00 +0700

    task module init

---
+++ app/views/tasks/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @task, :id, :name, :subject_id, :created_at, :updated_at
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             supervisor: true)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
+
+50.times do |n|
+  name  = Faker::Lorem.sentence 5
+  instruction = Faker::Lorem.sentence 50
+  date1 = Faker::Time.between(2.weeks.ago, Time.now) 
+  date2 = Faker::Time.between(2.weeks.ago, Time.now)
+  Subject.create(name: name,
+                 instruction: instruction,
+  	             start_date: [date1,date2].min,
+                 end_date: [date1,date2].max)
+end
+
+100.times do |n|
+  name = Faker::Lorem.sentence 5
+  subject = Subject.all[(0..30).to_a.shuffle.first]
+  Task.create(name: name,
+  						subject: subject)
+end
